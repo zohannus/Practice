d5f50c3c6836ef32e0ac93e09fa0f7e6d0e34111
kimhyeungjo <zohannus@badbeans.dev>
2025-02-22 11:13:13 +0900

    0221

--- HangMAn/HangMAn/Hangman2.cs
+++ HangMAn/HangMAn/Hangman2.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 
-class HangMan
+class Hangman2
 {
-    Dictionary<int, char> Word = null;
+    Dictionary<int, string> Word = null;
     public void Main()
     {
-        List<string> WordList = new List<string>() { "apple", "banana" };
-        Word = new Dictionary<int, char>();
+        List<string> WordList = new List<string>();
+        Word = new Dictionary<int, string>()
+        {
+            {0, "apple"},
+            {1, "banana"}
+        };
         int Life = 10;
         string Answer = "";
         int randomValue;
-        
+
         //--- 랜덤값으로 단어선택
         Random randomNum = new Random();
         randomValue = randomNum.Next(2);
         if (randomNum != null)
-            Answer = WordList[randomValue];
+            Answer = Word[randomValue];
+
+        foreach (var word in Answer)
+        {
+            WordList.Add(word.ToString());
+        }
         //--- 랜덤값으로 단어 선택
 
         for (int i = 0; i < Answer.Length; i++)
@@ -32,30 +40,30 @@ class HangMan
 
         Console.WriteLine($"정답은? 남은횟수 :{Life}");
 
-        //---단어 정답 체크
-        int Index = 0;
-        foreach (var Alpha in Answer)
-        {
-            
-            Word.Add(Index, Alpha);
-            Index++;
-        }
-        for (int i = 0; i < Word.Count; i++)
-        {
-            Console.WriteLine(Word[i]);
-        }
+        //--- 단어 정답 체크
+        //foreach (var Alpha in Answer)
+        //{
+        //    if (Word.ContainsKey(Alpha))
+        //    {
+        //        Word[Alpha]++;
+        //    }
+        //    else
+        //    {
+        //        Word[Alpha] = 1;
+        //    }
+        //}
+
         while (true)
         {
             string Chat = Console.ReadLine();
-            char CharChat = Chat[0];
-            if (Word.ContainsValue(CharChat))
+            if (Word.ContainsValue(Chat))
             {
-                Console.WriteLine($"포함하고 있습니다.");
+                Console.WriteLine($"굿");
                 for (int i = 0; i < Answer.Length; i++)
                 {
-                    if (CharChat == Word[i])
+                    if (WordList[i]== Chat)
                     {
-                        Console.Write(CharChat);
+                        Console.Write(Chat);
                     }
                     else
                         Console.Write("ㅡ");
@@ -71,10 +79,4 @@ class HangMan
             }
         }
     }
-
-
-
-        
-    
 }
-
--- HangMAn/HangMAn/MainGate.cs
+++ HangMAn/HangMAn/MainGate.cs
@@ -1,9 +1,12 @@
 ﻿class MainGate
 {
     static HangMan hangMan = null;
+    static Hangman2 hangMan2 = null;
     static void Main()
     {
         hangMan = new HangMan();
+        hangMan2 = new Hangman2();
         hangMan.Main();
+        //hangMan2.Main();
     }
 }
